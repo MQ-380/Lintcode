f9b4eb680f8ec25570bc40ff11703ed67e97f6f1
shmao <shmao288@gmail.com>
2015-10-23 13:16:18 +0800

    update anagram.cpp

--- 阶梯训练/大公司面试/anagram.cpp
+++ 阶梯训练/大公司面试/anagram.cpp
@@ -1,16 +1,17 @@
-/*写出一个函数 anagram(s, t) 去判断两个字符串是否是颠倒字母顺序构成的
-样例
-给出 s="abcd"，t="dcab"，返回 true
-*/
+//写出一个函数 anagram(s, t) 去判断两个字符串是否是颠倒字母顺序构成的
+//样例
+//给出 s="abcd"，t="dcab"，返回 true
+
+
 //因为如果符合条件则排序后的内容相同，即可判断。
 //所用STL：sort():
 class Solution {
 public:
-    /**
-     * @param s: The first string
-     * @param b: The second string
-     * @return true or false
-     */
+    
+     //@param s: The first string
+     //@param b: The second string
+     //@return true or false
+     
     bool anagram(string s, string t) {
         if(s.size()!=t.size()) return false;
         sort(s.begin(),s.end()); 
